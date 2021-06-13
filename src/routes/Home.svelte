41a8e1bdc3c7fa5dc7a7ae3e12a7cf38470e59a7
MohamadTurki <62876978+MohamadTurki@users.noreply.github.com>
2021-06-13 13:14:48 +0300

    various edits (#2)
    * various edits
    
    * removed uneeded npm lock file
    
    Co-authored-by: Raafat Turki <raafat.turki@protonmail.com>

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -30,39 +30,50 @@
 		{ val: "2", func: insert },
 		{ val: "3", func: insert },
 		{ val: "+", func: insert },
+		{ val: "-", func: insert },
+
 		{ val: "4", func: insert },
 		{ val: "5", func: insert },
 		{ val: "6", func: insert },
-		{ val: "-", func: insert },
+		{ val: "^", func: insert },
+		{ val: "*", func: insert },
+
 		{ val: "7", func: insert },
 		{ val: "8", func: insert },
 		{ val: "9", func: insert },
-		{ val: "^", func: insert },
-		{ val: "*", func: insert },
 		{ val: "/", func: insert },
-		{ val: "0", func: insert },
-		{ val: "=", func: calc },
-		{ val: "DER", func: deriv },
-		{ val: "x", func: insert },
 		{ val: "%", func: insert },
+
+		{ val: "=", func: calc },
+		{ val: "0", func: insert },
+		{ val: ".", func: insert },
+		{ val: "DEL", func: del },
+		{ val: "AC", func: all_clear },
+
 		{ val: "sin", func: insert },
 		{ val: "cos", func: insert },
 		{ val: "tan", func: insert },
-		{ val: "DEL", func: del },
-		{ val: "AC", func: all_clear },
 		{ val: "(", func: insert },
 		{ val: ")", func: insert },
+
+		{ val: "abs", func: insert },
+		{ val: "sqrt", func: insert },
+		{ val: "log", func: insert },
+		{ val: "x", func: insert },
+		{ val: "DER", func: deriv },
 	]
 </script>
 
-<div class ="calc">
-<h2>{expr == "" ? ":D" : expr}</h2>
-	<h1>{res}</h1>
+<div class="container">
+	<div class ="calc">
+	<h2>{expr == "" ? ":D" : expr}</h2>
+		<h1>{res}</h1>
 
-	<div class="container">
-		{#each buttons as btn}
-			<button on:click={() => btn.func(btn.val)}>{btn.val}</button>
-		{/each}
+		<div class="buttons">
+			{#each buttons as btn}
+				<button on:click={() => btn.func(btn.val)}>{btn.val}</button>
+			{/each}
+		</div>
 	</div>
 </div>
 
@@ -72,42 +83,44 @@
 		font-weight: 400;
 	}
 
-	:global(body) {
+	.container {
+		width: 100vw;
+		height: 100vh;
 		background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+		display: flex;
 	}
 
 	.calc {
 		border-radius: 20px;
 		background: rgba(255, 255, 255, 0.4);
-		margin: 100px auto;
-		width: 480px;
+		margin: auto;
 		padding: 20px;
 	}
 
-	.container {
-		display: grid ;
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		grid-template-rows: 1fr 1fr 1fr 1fr;
+	.buttons {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+		grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
+
+		button {
+			background: rgba(255, 255, 255, 0.4);
+			padding: 10px; 
+			font-size: 28px;
+			background: transparent;
+			color: white;
+			border: none;
+			border-radius: 5px;
+
+			&:hover {
+				background: rgba(255, 255, 255, 0.5);
+			}
+
+			&:active {
+				background: rgba(255, 255, 255, 0.75);
+			}
+		}
 	}
 
-	.container button{
-		padding: 20px; 
-		font-size: 34px;
-		background: transparent;
-		color: white;
-		border: none;
-		border-radius: 70px;
-
-	&:hover {
-		background: rgba(255, 255, 255, 0.5);
-	}
-
-	&:active {
-		background: rgba(255, 255, 255, 0.75);
-		
-	}
-
-}
 
 	h1, h2 {
 		color: white;
--- src/routes/NotFound.svelte
+++ src/routes/NotFound.svelte
@@ -1,4 +1,33 @@
-<center>
-	<h1>404 Not Found</h1>
-</center>
+<script>
+	let title = "404"
+	let desc = "Opps! Page not found"
+	let warn = "Sorry, the page you're looking for doesn't exist. If you think something is broken, report a problem"
+</script>
 
+<div class="container">
+	<div class="content">
+		<h2 class="title" data-text={title}> {title} </h2>
+		<h4 data-text={desc}> {desc} </h4>
+		<br>
+		<h4 data-text={warn}> {warn} </h4>
+	</div>
+</div>
+
+<style lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.content {
+  max-width: 600px;
+  text-align: center;
+}
+
+.title {
+  font-size: 20vw;
+}
+</style>
