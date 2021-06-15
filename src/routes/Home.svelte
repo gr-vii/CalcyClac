0f36e48f85c837265b7e5265d3c5e282079381b3
Raafat Turki <raafat.turki@protonmail.com>
2021-06-15 18:56:46 +0300

    added expandable layout and dynamic grid

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,8 +1,11 @@
 <script>
 	import {derivative, evaluate} from "mathjs"
 	
+	let is_expanded = true
 	let expr = ""
 	let res = 0
+	let cols = 5
+	let rows = 6
 
 	function insert(seg) {
 		expr += seg
@@ -25,42 +28,51 @@
 		expr = expr.substring(0, expr.length-1)
 	}
 
+	function toggle_expanded() {
+		is_expanded = !is_expanded
+		cols = 5
+		rows = 5
+	}
+
+	/* { val: String", func: Function, w Number, h Number } */
 	let buttons = [
-		{ val: "1", func: insert },
-		{ val: "2", func: insert },
-		{ val: "3", func: insert },
-		{ val: "+", func: insert },
-		{ val: "-", func: insert },
+		{ val: "1",		func: insert	},
+		{ val: "2",		func: insert	},
+		{ val: "3",		func: insert	},
+		{ val: "+",		func: insert	},
+		{ val: "-",		func: insert	},
 
-		{ val: "4", func: insert },
-		{ val: "5", func: insert },
-		{ val: "6", func: insert },
-		{ val: "^", func: insert },
-		{ val: "*", func: insert },
+		{ val: "4",		func: insert	},
+		{ val: "5",		func: insert	},
+		{ val: "6",		func: insert	},
+		{ val: "^",		func: insert	},
+		{ val: "*",		func: insert	},
 
-		{ val: "7", func: insert },
-		{ val: "8", func: insert },
-		{ val: "9", func: insert },
-		{ val: "/", func: insert },
-		{ val: "%", func: insert },
+		{ val: "7",		func: insert	},
+		{ val: "8",		func: insert	},
+		{ val: "9",		func: insert	},
+		{ val: "/",		func: insert	},
+		{ val: "%",		func: insert	},
 
-		{ val: "=", func: calc },
-		{ val: "0", func: insert },
-		{ val: ".", func: insert },
-		{ val: "DEL", func: del },
-		{ val: "AC", func: all_clear },
+		{ val: "=",		func: calc,		w: 2},
+		{ val: "0",		func: insert	},
+		{ val: ".",		func: insert	},
+		{ val: "DEL",	func: del		},
+		{ val: "AC",	func: all_clear },
 
-		{ val: "sin", func: insert },
-		{ val: "cos", func: insert },
-		{ val: "tan", func: insert },
-		{ val: "(", func: insert },
-		{ val: ")", func: insert },
+		{ val: "sin",	func: insert,	advanced: true},
+		{ val: "cos",	func: insert,	advanced: true},
+		{ val: "tan",	func: insert,	advanced: true},
+		{ val: "(",		func: insert,	advanced: true},
+		{ val: ")",		func: insert,	advanced: true},
 
-		{ val: "abs", func: insert },
-		{ val: "sqrt", func: insert },
-		{ val: "log", func: insert },
-		{ val: "x", func: insert },
-		{ val: "DER", func: deriv },
+		{ val: "abs",	func: insert,	advanced: true},
+		{ val: "sqrt",	func: insert,	advanced: true},
+		{ val: "log",	func: insert,	advanced: true},
+		{ val: "x",		func: insert,	advanced: true},
+		{ val: "DER",	func: deriv	,	advanced: true},
+
+		{ val: "EXP",	func: toggle_expanded },
 	]
 </script>
 
@@ -69,9 +81,22 @@
 	<h2>{expr == "" ? ":D" : expr}</h2>
 		<h1>{res}</h1>
 
-		<div class="buttons">
+		<div
+			class="buttons"
+			style="
+			grid-template-columns: repeat({cols}, 1fr);
+			grid-template-rows: repeat({rows}, 1fr);
+			"
+		>
 			{#each buttons as btn}
-				<button on:click={() => btn.func(btn.val)}>{btn.val}</button>
+				<button
+				on:click={() => btn.func(btn.val)}
+				style="
+				grid-column: span {btn.w ? btn.w : 1};
+				grid-row: span {btn.h ? btn.h : 1};
+				display: { btn.advanced && !is_expanded ? "none" : "grid" };
+				"
+				>{btn.val}</button>
 			{/each}
 		</div>
 	</div>
@@ -99,8 +124,6 @@
 
 	.buttons {
 		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-		grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
 
 		button {
 			background: rgba(255, 255, 255, 0.4);
